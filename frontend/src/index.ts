64f19ad78dc80d1b57a35cd4873a07e84e34f995
johnyworld <johnyworld@naver.com>
2020-09-21 02:03:13 +0900

    Fix import socket.io errors with typescript.
    - using cdn and just declare socket.io package

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,4 +1,10 @@
 import hi from './hello/hello.js';
-console.log('start!');
+declare const io: typeof import('socket.io');
+
+const socket = io('http://localhost:7000');
+
+socket.emit('hello');
+socket.on('connect', () => console.log('connexted!!!'));
+console.log('start!!sa!');
 hi();
 
