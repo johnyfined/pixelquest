039caae6f1e07443a520e99db23f07a0068c5f78
johnyworld <johnyworld@naver.com>
2020-09-21 02:15:53 +0900

    Add socket test code

--- server/server.ts
+++ server/server.ts
@@ -3,5 +3,10 @@ import * as socketio from "socket.io";
 let io = socketio(7000);
 
 io.on("connection", function(socket: any) {
+  socket.on('hello', () => {
+    console.log('hello come')
+    socket.emit('connect');
+  })
+
   console.log("a user connected");
 });
